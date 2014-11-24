8e9139bc0303657c25b60551d65a04a97fd13377
Josh Wills <jwills@cloudera.com>
2014-11-23 17:33:40 -0800

    Add license headers

--- src/test/scala/com/cloudera/science/geojson/GeoJsonSpec.scala
+++ src/test/scala/com/cloudera/science/geojson/GeoJsonSpec.scala
@@ -1,3 +1,17 @@
+/**
+ * Copyright (c) 2014, Cloudera, Inc. All Rights Reserved.
+ *
+ * Cloudera, Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"). You may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * This software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, either express or implied. See the License for
+ * the specific language governing permissions and limitations under the
+ * License.
+ */
 package com.cloudera.science.geojson
 
 import com.esri.core.geometry.Point
@@ -8,11 +22,11 @@ import GeoJsonProtocol._
 class GeoJsonSpec extends FlatSpec with Matchers {
   val geojson = scala.io.Source.fromURL(getClass.getResource("/boroughs.geojson")).mkString
 
-  "GeoJson" should "correctly parse the NYC boroughs into usable Geometry objects" in {
+  "GeoJson" should "correctly parse the NYC boroughs into usable RichGeometry objects" in {
     val features = geojson.parseJson.convertTo[FeatureCollection]
     val point = new Point(-73.994499, 40.75066)
     val b = features.filter(f => f.geometry.contains(point))
-    b.map(f => f.properties("borough").convertTo[String]) should be(Array("Manhattan"))
+    b.map(_.properties("borough").convertTo[String]).head should be("Manhattan")
   }
 
   "GeoJson" should "parse GeoJSON into objects and back again" in {
